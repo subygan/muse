51c06975fc387431c84b5af9bd5c18866544d89b
Suriya Ganesh <suriyaganesh097@gmail.com>
2021-12-11 14:49:37 +0530

    changes

--- site/js/app.js
+++
@@ -1,9 +0,0 @@
-console.log("newwewe")
-
-window.addEventListener('beforeinstallprompt', e => {
-    console.log('beforeinstallprompt Event fired');
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    this.deferredPrompt = e;
-    return false;
-});
