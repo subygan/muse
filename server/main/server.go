ed0875b05a2206d0a71dd475ce9f1c6b91598aa7
Suriya Ganesh <suriyaganesh097@gmail.com>
2021-12-11 18:53:43 +0530

    Fucking working thinggit add .! 🪐

--- server/main/server.go
+++ server/main/server.go
@@ -6,13 +6,14 @@ import (
 	"fmt"
 	. "github.com/WAY29/icecream-go/icecream"
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"log"
-	"net/http"
-	"os"
-	"path/filepath"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
 
@@ -34,14 +35,50 @@ func iterate(path string) {
 	}
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request){
-	Ic("Working??")
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		Ic("Fatal error")
+func getAllKeys(client *s3.Client) ([]*string, int32) {
+	bucketName:= "suriya-music"
+	params := &s3.ListObjectsV2Input{
+		Bucket: &bucketName,
 	}
+	// Create the Paginator for the ListObjectsV2 operation.
+	p := s3.NewListObjectsV2Paginator(client, params, func(o *s3.ListObjectsV2PaginatorOptions) {
+		if v := 100; v != 0 {
+			o.Limit = int32(v)
+		}
+	})
 
-	client := s3.NewFromConfig(cfg)
+	// Iterate through the S3 object pages, printing each object returned.
+	var i int
+	var count int32
+
+	var l []*string
+	log.Println("Objects:")
+	for p.HasMorePages() {
+		i++
+
+		// Next Page takes a new context for each page retrieval. This is where
+		// you could add timeouts or deadlines.
+		page, err := p.NextPage(context.TODO())
+		if err != nil {
+			log.Fatalf("failed to get page %v, %v", i, err)
+		}
+
+		count += page.KeyCount
+		// Log the objects found
+		for _, obj := range page.Contents {
+			fmt.Println("Object:", *obj.Key)
+			l = append(l, obj.Key)
+		}
+	}
+	fmt.Println("#### ", count)
+
+	return l, count
+}
+
+
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request){
+
 
 	//output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
 	//	Bucket: aws.String("suriya-music"),
@@ -50,16 +87,14 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request){
 	//	Ic(err)
 	//}
 
-	fmt.Println("####")
-
-	key1 := "04_Uproar.mp3"
-	i := s3.GetObjectInput{
+	key1 := obj.keys[rand.Intn(int(obj.count))]
+	h := s3.GetObjectInput{
 		Bucket: aws.String("suriya-music"),
-		Key: &key1,
+		Key: key1,
 	}
-	psClient := s3.NewPresignClient(client)
+	psClient := s3.NewPresignClient(obj.Client)
 
-	Res, _ := psClient.PresignGetObject(context.TODO(), &i)
+	Res, _ := psClient.PresignGetObject(context.TODO(), &h)
 
 	Ic(Res)
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
@@ -67,7 +102,7 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request){
 
 	fmt.Println("Endpoint Hit: returnAllArticles")
 	json.NewEncoder(w).Encode(Url{
-		Url: Res.URL,
+		Url: "fuck you",
 	})
 }
 
@@ -76,33 +111,32 @@ type Url struct {
 	Url string `json:"Url"`
 }
 
+
+type Obj struct {
+	Client *s3.Client
+	keys []*string
+	count int32
+}
+
+var obj Obj
+
 func main() {
+	Ic("Working??")
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		Ic("Fatal error")
+	}
 
+	client := s3.NewFromConfig(cfg)
+	obj.Client = client
 
+	l, count := getAllKeys(client)
+	obj.keys = l
+	obj.count = count
 
 	myRouter := mux.NewRouter().StrictSlash(true)
 	// replace http.HandleFunc with myRouter.HandleFunc
 	myRouter.HandleFunc("/", returnAllArticles)
-	// finally, instead of passing in nil, we want
-	// to pass in our newly created router as the second
-	// argument
+
 	log.Fatal(http.ListenAndServe(":3000", myRouter))
-
-
-
-	//currentDirectory, err := os.Getwd()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//iterate(currentDirectory+"/../../site/music")
-    //fs := http.FileServer(http.Dir("../../site"))
-    //
-	//http.Handle("/", fs)
-	//
-	//log.Println("Listening on :3000...")
-	//err = http.ListenAndServe(":3000", nil)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
 }
